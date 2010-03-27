c2e1aee198272b57730280615d2e72836198a730
Eric Budd <ericbudd@calamitylane.com>
2010-03-26 23:23:09 -0500

    Refactoring code I have no idea why I made a class out of that, except that it provided a cutesy call.

--- chicken_little.rb
+++ chicken_little.rb
@@ -19,15 +19,13 @@ CONFIG = {
   :authtype => :login
 }
 
-class FallingSky
-  def self.squawk(url, response)
-    body = "Subject: [#{url.to_s}] is falling!\n"
-    body += "OH NOES!!!\n  I expected a 200 from #{url.to_s} but got a #{response.to_s} instead!"
+SMTP_SPLAT = [CONFIG[:smtp_server], CONFIG[:port], CONFIG[:domain], CONFIG[:account], CONFIG[:password], CONFIG[:authtype]]
 
-    Net::SMTP.start(CONFIG[:smtp_server], CONFIG[:port], CONFIG[:domain], CONFIG[:account], CONFIG[:password], CONFIG[:authtype]) do |sender|
-      sender.sendmail(body, CONFIG[:sender], CONFIG[:recipients])
-    end
-  end
+def squawk(url, response)
+  body = "Subject: [#{url.to_s}] is falling!\n"
+  body += "OH NOES!!!\n  I expected a 200 from #{url.to_s} but got a #{response.to_s} instead!"
+
+  Net::SMTP.start(*SMTP_SPLAT) { |sender| sender.sendmail(body, CONFIG[:sender], CONFIG[:recipients]) }
 end
 
 CONFIG[:urls_to_check].each do |url|
@@ -37,6 +35,6 @@ CONFIG[:urls_to_check].each do |url|
     response_code = e
   ensure
     puts response_code.to_s
-    FallingSky.squawk(url, response_code) unless response_code == '200'
+    squawk(url, response_code) unless response_code == '200'
   end
 end
